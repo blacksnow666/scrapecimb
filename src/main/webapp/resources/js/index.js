5eacbf60518508fb924fc866bc85b6f46f188afa
Titi Wangsa bin Damhore <blacksnow666@gmail.com>
2014-08-26 13:40:57 +0800

    show prev auction

--- src/main/webapp/resources/js/index.js
+++ src/main/webapp/resources/js/index.js
@@ -9,7 +9,42 @@ $(function() {
 		});
 	});
 	$("table").on("click", "a.link-auction-count", function() {
+		function success(data) {
+			var dialog = $("#dialog-info");
+			var container = $(".container-fluid", dialog);
+			container.empty();
+			var count = data.length;
+			var n;
+			var template = $(".row-template", dialog);
+			for (n = 0; n < count - 1; n++) {
+				var row = template.clone();
+				row.removeClass("row-template");
+				$(".col-date", row).text(data[n].date);
+				$(".col-price", row).text(data[n].price);
+				container.append(row);
+			}
+			dialog.dialog("open");
+		}
 
+		var span = $("span.link-id", $(this));
+		var id = span.text();
+		var form = $("#form-auction-history");
+		$("input.value", form).val(id);
+		var options = {
+			"success" : success
+		};
+		form.ajaxSubmit(options);
+		return false;
+	});
+
+	$("#dialog-info").dialog({
+		"autoOpen" : false,
+		"buttons" : [ {
+			text : "Ok",
+			click : function() {
+				$(this).dialog("close");
+			}
+		} ]
 	});
 
 });
